e9580cbf549d67ba4410186959a9361ed41d718b
Bombel <bombel@bombel.com>
2020-03-04 11:17:59 +0100

    Add selector in tripsRedux.js

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -24,6 +24,7 @@ export const getTripById = ({trips}, tripId) => {
   const filtered = trips;
 
   // TODO - filter trips by tripId
+  filtered.filter(trips => trips.tripId == tripId);
 
   console.log('filtering trips by tripId:', tripId, filtered);
   return filtered.length ? filtered[0] : {error: true};
@@ -33,6 +34,7 @@ export const getTripsForCountry = ({trips}, countryCode) => {
   const filtered = trips;
 
   // TODO - filter trips by countryCode
+  filtered.filter(trips => trips.countryCode == countryCode);
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
   return filtered.length ? filtered : [{error: true}];
