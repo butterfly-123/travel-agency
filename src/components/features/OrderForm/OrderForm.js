d437c7e043800c4b72ba8f83b473e038e842abab
Bombel <bombel@bombel.com>
2020-03-05 15:29:22 +0100

    Fix with order form

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import OrderSummary from '../OrderSummary/OrderSummary';
 import { Grid, Row, Col } from 'react-flexbox-grid';
-import PageTitle from '../../common/PageTitle/PageTitle';
 
 class OrderForm extends React.Component {
   render() {
@@ -9,7 +8,6 @@ class OrderForm extends React.Component {
       <Grid>
         <Row>
           <Col xs={12}>
-            <PageTitle text='Trip options' />
             <OrderSummary />
           </Col>
         </Row>
