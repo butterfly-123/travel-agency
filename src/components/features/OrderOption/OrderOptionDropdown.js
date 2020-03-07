d7e07202a8d200f2ab52136c8f69748f7b4feaed
Bombel <bombel@bombel.com>
2020-03-07 15:34:52 +0100

    Add 4 components in OrderOption

---
+++ src/components/features/OrderOption/OrderOptionDropdown.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+
+const OrderOptionDropdown = () => {
+  <div className={styles.dropdown}>
+    OrderOptionDropdown
+  </div>;
+};
+
+export default OrderOptionDropdown;
---
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import styles from './OrderOption.scss';
+
+const OrderOptionIcons = () => {
+  <div className={styles.icon}>
+    OrderOptionIcons
+  </div>;
+};
+
+export default OrderOptionIcons;
