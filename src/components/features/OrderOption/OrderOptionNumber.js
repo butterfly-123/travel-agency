891c553938375d3d68d917d6f0e9651f476932a4
Bombel <bombel@bombel.com>
2020-03-08 20:28:34 +0100

    Add options Icon and Number

--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
 
-const OrderOptionNumber = () => (
-  <div className={styles.input}>
-    OrderOptionNumber
+const OrderOptionNumber = (limits, currentValue, setOptionValue) => (
+  <div className={styles.number}>
+    <input type='number'
+      className={styles.inputSmall}
+      value={currentValue}
+      min={limits.min}
+      max={limits.max}
+      onChange={event => setOptionValue(event.currentTarget.value)}
+    ></input>
   </div>
 );
+
+OrderOptionNumber.propTypes = {
+  currentValue: PropTypes.string,
+  setOptionValue: PropTypes.func,
+  limits: PropTypes.object,
+};
 
 export default OrderOptionNumber;
