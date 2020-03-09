7baf7ba5f8a03ad670d112ff62d90b5de4a0231b
Bombel <bombel@bombel.com>
2020-03-09 09:57:17 +0100

    Add change in OrderOptionDate to work and add propTypes in OrderOption

--- src/components/features/OrderOption/OrderOption.js
+++ src/components/features/OrderOption/OrderOption.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './OrderOption.scss';
 import OrderOptionDropdown from './OrderOptionDropdown';
 import OrderOptionIcons from './OrderOptionIcons';
@@ -31,6 +32,13 @@ const OrderOption = ({name, type, id, setOrderOption, ...otherProps}) => {
       </div>
     );
   }
+};
+
+OrderOption.propTypes = {
+  name: PropTypes.string,
+  type: PropTypes.string,
+  id: PropTypes.string,
+  setOrderOption: PropTypes.func,
 };
 
 export default OrderOption;
--- src/components/features/OrderOption/OrderOptionIcons.js
+++ src/components/features/OrderOption/OrderOptionIcons.js
@@ -18,7 +18,11 @@ const OrderOptionIcons = ({ values, required, currentValue, setOptionValue }) =>
     )}
 
     {values.map(value => (
-      <div className={styles.icon} key={value.id} onClick={() => setOptionValue(value.id)}
+      <div
+        className={currentValue === value.id ? styles.iconActive : styles.icon}
+        key={value.id}
+        value={value.id}
+        onClick={() => (setOptionValue(value.id))}
       >
         <Icon name={value.icon} />
         {value.name}
