6939e0a6f0ebcbc4da26ce811bdb0f0c69df65fe
Bombel <bombel@bombel.com>
2020-03-10 10:09:38 +0100

    Add OrderOptionDate and OrderOptionText

---
+++ src/components/features/OrderOption/OrderOptionText.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class OrderOptionText extends React.Component {
+  render() {
+    return (
+      <input type='text'></input>
+    );
+  }
+}
+
+export default OrderOptionText;
